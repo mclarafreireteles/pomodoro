8eb35f3bf0b8cc4e01dae0e8fbf38cbc0586b08e
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-05-23 21:32:47 -0300

    :sparkles: feat: add pomodoro timer type selection buttons

--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,58 @@
+import { useState } from "react";
 import { Image, StyleSheet, View, Text, Pressable } from "react-native";
 
+const pomodoro = [
+  {
+    id: 'focus',
+    initialValue: 25,
+    image: require('./pomodoro.svg')
+  },
+  {
+    id: 'short',
+    initialValue: 5,
+    image: require('./short.svg')
+  },
+  {
+    id: 'long',
+    initialValue: 15,
+    image: require('./short.svg')
+  },
+]
+
 export default function Index() {
+
+  const [timerType, setTimerType] = useState(pomodoro[0])
+
   return (
     <View
       style={styles.container}
     >
-      <Image source={require('./pomodoro.svg')} />
+      <Image source={timerType.image} style={styles.image}/>
       <View style={styles.actions}>
-
+      <View style={styles.context}>
+        <Pressable style={styles.contextButtonActive}>
+          <Text style={styles.contextButtonText}>
+            Foco
+          </Text>
+        </Pressable>
+        <Pressable>
+          <Text style={styles.contextButtonText}>
+            Pausa curta
+          </Text>
+        </Pressable>
+        <Pressable>
+          <Text style={styles.contextButtonText}>
+            Pausa longa
+          </Text>
+        </Pressable>
+      </View>
         <Text style={styles.timer}>
           25:00
         </Text>
         <Pressable style={styles.button}>
-          Começar
+          <Text style={styles.buttonText}>
+            Começar
+          </Text>
         </Pressable>
       </View>
     </View>
@@ -27,6 +67,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#021123",
     gap: 40
   },
+  image: {
+    width: '80%'
+  },
   actions: {
     paddingVertical: 24,
     paddingHorizontal: 24,
@@ -34,16 +77,37 @@ const styles = StyleSheet.create({
     width: '80%',
     borderRadius: 32,
     borderWidth: 2,
-    borderColor: '#144480'
+    borderColor: '#144480',
+    gap: 32,
+  },
+  context: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+
+  },
+  contextButtonActive: {
+    backgroundColor: '#144480',
+    borderRadius: 8
+  },
+  contextButtonText: {
+    fontSize: 12.5,
+    color: '#FFF',
+    padding: 8
   },
   timer: {
     fontSize: 54,
     color: '#FFFFFF',
     textAlign: 'center',
+    fontWeight: 'bold'
   },
   button: {
     backgroundColor: '#B872FF',
     borderRadius: 32,
     padding: 8,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#021123'
   }
 })
